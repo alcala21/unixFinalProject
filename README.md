b493c4cef9ebb89f4f9adf8faa6abab250020522
Carlos Alcala <Carlos.Alcala@jci.com>
2017-11-18 14:14:28 -0600

    Modified the makefile.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Guessing Game
-Sat Nov 18 13:59:22 CST 2017
-
-The file guessinggame.sh has the following number of lines:
-10
+# The Unix Workbench Final Project. \n
+The make command was run on: \n
+Sat Nov 18 14:13:25 CST 2017
+\n
+The file guessinggame.sh has the following number of lines: \n
+11
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,11 @@
 all: README.md
 
 README.md:
-  echo "# Guessing Game" > README.md
+  echo "# The Unix Workbench Final Project \n" > README.md
+  echo "The make command was run on: \n" >> README.md
   date >> README.md
   echo "\n" >> README.md
-  echo "The file guessinggame.sh the following number of lines:" >> README.md
+  echo "The file guessinggame.sh has the following number of lines: \n" >> README.md
   wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
 
 clean:
